637a0deb51e239c61dfd4ee61fce7e79dbc42c32
Raul BUIBAS (contractor) <raul.buibas@amadeus.com>
2018-06-11 12:43:38 +0200

    Changes to recipe management: - dynamically provide details about the recipe - move recipe iteration inside the recipe-list component

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -8,15 +8,24 @@ import { Recipe } from '../recipe.model';
 })
 export class RecipeListComponent implements OnInit {
 
+  @Output() highRecipeSelected = new EventEmitter<Recipe>();
+
   recipes: Recipe[] = [
     new Recipe('Rachel\'s \"Traditional\" English Trifle', 
                'Custard, good. Jam, good. Meat, good!', 
-               'https://i.pinimg.com/originals/9b/77/41/9b77416e98022213ce03a08043538747.jpg')
+               'https://i.pinimg.com/originals/9b/77/41/9b77416e98022213ce03a08043538747.jpg'),
+    new Recipe('Tartiflette', 
+               'Like we had when we went sking in the Alps.', 
+               'https://image.afcdn.com/recipe/20160401/38946_w420h344c1cx2690cy1793.jpg')               
   ];
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  higherRecipeSelected(recipeToEmit: Recipe) {
+    this.highRecipeSelected.emit(recipeToEmit);
   }
 
 }
