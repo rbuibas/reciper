ea52effd55a5501e1e4095afcac6489d179c3d40
Raul BUIBAS (contractor) <raul.buibas@amadeus.com>
2018-06-11 13:08:32 +0200

    Functionality for adding the ingredients to the list of ingredients

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,4 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { 
+  Component, 
+  OnInit, 
+  ElementRef, 
+  ViewChild,
+  EventEmitter, 
+  Output} from '@angular/core';
+import { Ingredient } from '../../shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping-edit',
@@ -7,9 +14,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ShoppingEditComponent implements OnInit {
 
+  @ViewChild('nameInput') nameInputRef: ElementRef;
+  @ViewChild('ammountInput') ammountInputRef: ElementRef;
+
+  @Output() ingredientAdded = new EventEmitter<Ingredient>();
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onAddClick() {
+    const tempName = this.nameInputRef.nativeElement.value;
+    const tempAmmount = this.ammountInputRef.nativeElement.value;
+    const newIngredient = new Ingredient(tempName, tempAmmount);
+    this.ingredientAdded.emit(newIngredient);
+  }
+
+  onDelClick() {
+
+  }
+
+  onClrClick() {
+
   }
 
 }
