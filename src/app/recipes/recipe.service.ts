a0a1bde9329c977ad735f9b73d84d9ad18b1b13d
Raul BUIBAS (contractor) <raul.buibas@amadeus.com>
2018-06-11 17:27:52 +0200

    Add ingredients to a recipe and display them under the recipse selected

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,5 +1,6 @@
 import { Injectable, EventEmitter } from '@angular/core';
 import { Recipe } from './recipe.model';
+import { Ingredient } from '../shared/ingredient.model';
 
 @Injectable({
   providedIn: 'root'
@@ -11,10 +12,21 @@ export class RecipeService {
   private recipes: Recipe[] = [
     new Recipe('Rachel\'s \"Traditional\" English Trifle', 
                'Custard, good. Jam, good. Meat, good!', 
-               'https://i.pinimg.com/originals/9b/77/41/9b77416e98022213ce03a08043538747.jpg'),
+               'https://i.pinimg.com/originals/9b/77/41/9b77416e98022213ce03a08043538747.jpg',
+              [
+                new Ingredient('Meat', 2),
+                new Ingredient('Custard', 10),
+                new Ingredient('Jam', 2),
+                new Ingredient('Eggs', 4)
+              ]),
     new Recipe('Tartiflette', 
                'Like we had when we went sking in the Alps.', 
-               'https://image.afcdn.com/recipe/20160401/38946_w420h344c1cx2690cy1793.jpg')               
+               'https://image.afcdn.com/recipe/20160401/38946_w420h344c1cx2690cy1793.jpg',
+              [
+                new Ingredient('Patatas', 5),
+                new Ingredient('Cheese', 2),
+                new Ingredient('Olives', 12)
+              ])               
   ];
 
   // only get a copy
