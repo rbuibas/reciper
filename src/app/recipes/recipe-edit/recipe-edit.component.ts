c097876d2f0a461da31c03678c82d12890cee769
Raul BUIBAS (contractor) <raul.buibas@amadeus.com>
2018-06-21 16:32:44 +0200

    Connect the HTML template to the logic (this is reactive form generatio)

--- src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
+import { FormGroup, FormControl } from '@angular/forms';
+import { RecipeService } from '../recipe.service';
 
 @Component({
   selector: 'app-recipe-edit',
@@ -10,8 +12,11 @@ export class RecipeEditComponent implements OnInit {
 
   id: number;
   editMode = false;
+  recipeForm: FormGroup;
 
-  constructor(private route: ActivatedRoute) { }
+  constructor(
+      private route: ActivatedRoute,
+      private recSrv: RecipeService) { }
 
   ngOnInit() {
     this.route.params
@@ -19,8 +24,30 @@ export class RecipeEditComponent implements OnInit {
         (params: Params) => {
           this.id = +params['id'];
           this.editMode = params['id'] != null; // if we are in edit mode, the ID is present, otherwise not
+          this.initForm();
         }
       );
+  }
+
+  private initForm() {
+    let recipeName = '';
+    let imagePath = '';
+    let description = '';
+    if (this.editMode) {
+      const recipe = this.recSrv.getRecipe(this.id);
+      recipeName = recipe.name;
+      imagePath = recipe.imagePath;
+      description = recipe.description;
+    }
+    this.recipeForm = new FormGroup({
+      'name': new FormControl(recipeName),
+      'imagePath': new FormControl(imagePath),
+      'desc': new FormControl(description),
+    });
+  }
+
+  onSubmit() {
+    console.log(this.recipeForm);
   }
 
 }
