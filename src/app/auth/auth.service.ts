a6ce948e371b62ce616abcd9d439efb13f83d410
Raul BUIBAS (contractor) <raul.buibas@amadeus.com>
2018-06-27 12:55:20 +0200

    Add SignIn functionaility

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -9,9 +9,15 @@ export class AuthService {
   constructor() { }
 
   signupUser(email: string, password: string) {
-    firebase.auth().createUserAndRetrieveDataWithEmailAndPassword(email, password)
+    firebase.auth().createUserWithEmailAndPassword(email, password)
       .catch(
         error => console.error(error));
+  }
+
+  signinUser(email: string, password: string) {
+    firebase.auth().signInWithEmailAndPassword(email, password)
+      .then(response => console.log(response))
+      .catch(error => console.error(error));
   }
 
 }
