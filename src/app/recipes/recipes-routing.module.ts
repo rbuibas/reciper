0814f3e8a926ae9cc587dc0bc7331f0ff5256efb
Raul BUIBAS (contractor) <raul.buibas@amadeus.com>
2018-06-27 17:00:50 +0200

    Fix !

--- src/app/recipes/recipes-routing.module.ts
+++ src/app/recipes/recipes-routing.module.ts
@@ -4,7 +4,7 @@ import { RecipesComponent } from './recipes.component';
 import { RecipeStartComponent } from './recipe-start/recipe-start.component';
 import { RecipeEditComponent } from './recipe-edit/recipe-edit.component';
 import { RecipeDetailComponent } from './recipe-detail/recipe-detail.component';
-import { AuthGuardService } from '../auth/AuthGuardService';
+import { AuthGuardService } from '../auth/auth-guard.service';
 
 const recipesRoutes: Routes = [
     {
