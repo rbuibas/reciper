a83201f9318d271dcb609a4b51a6233dad1b3edc
Raul BUIBAS (contractor) <raul.buibas@amadeus.com>
2018-06-27 16:35:59 +0200

    add shopping list module and auth module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,45 +1,35 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
-import { SnifferComponent } from './sniffer/sniffer.component';
-import { SmallSnifferComponent } from './sniffer/small-sniffer/small-sniffer.component';
-import { BigSnifferComponent } from './sniffer/big-sniffer/big-sniffer.component';
 import { HeaderComponent } from './header/header.component';
-import { ShoppingListComponent } from './shopping-list/shopping-list.component';
-import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
 import { ShoppingListService } from './shopping-list/shopping-list.service';
 import { AppRoutingModule } from './app-routing.module';
 import { RecipeService } from './recipes/recipe.service';
-import { SignupComponent } from './auth/signup/signup.component';
-import { SigninComponent } from './auth/signin/signin.component';
 import { AuthService } from './auth/auth.service';
 import { PersistenceService } from './shared/persistence.service';
 import { AuthGuardService } from './auth/auth-guard.service';
 import { RecipesModule } from './recipes/recipe.module';
 import { SharedModule } from './shared/shared.module';
+import { ShoppingListModule } from './shopping-list/shopping-list.module';
+import { AuthModule } from './auth/auth.module';
+import { SnifferModule } from './sniffer/sniffer.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SnifferComponent,
-    SmallSnifferComponent,
-    BigSnifferComponent,
-    HeaderComponent,
-    ShoppingListComponent,
-    ShoppingEditComponent,
-    SignupComponent,
-    SigninComponent
+    HeaderComponent
   ],
   imports: [
     BrowserModule, // contains the CommonModule as well
-    FormsModule,
     HttpModule,
     AppRoutingModule,
     RecipesModule,
-    SharedModule
+    ShoppingListModule,
+    AuthModule,
+    SharedModule,
+    SnifferModule
   ],
   providers: [
     ShoppingListService,
